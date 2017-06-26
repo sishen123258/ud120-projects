328f0822d3b0ccf5b4508423f204462c76ba8837
tong.yue <tong.yue@hypers.com>
2017-06-27 00:50:27 +0800

    naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,13 +9,13 @@
     Sara has label 0
     Chris has label 1
 """
-    
+import pickle
 import sys
 from time import time
 sys.path.append("../tools/")
-from email_preprocess import preprocess
-
-
+from tools.email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
@@ -26,8 +26,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+clf=GaussianNB()
+clf.fit(features_train, labels_train)
+res=clf.predict(features_test)
 
+length = range(0, len(res))
+acc=0
+for i in length:
+    if(res[i]==labels_test[i]):
+        acc=acc+1
+
+print acc/float(len(res))
 
 #########################################################
+
+# dicObj={1:1,2:2,3:3}
+# f=open("./pickle.txt","w")
+# pickle.dump(dicObj,f)
 
 
